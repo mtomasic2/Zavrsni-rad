43debe50436c8b34fd73ee52958416efee004fef
mtomasic2 <mtomasic20@student.foi.hr>
2023-07-26 12:02:48 +0200

    Pocetak implementacije generiranja OpenAPI specifikacije

---
+++ JS/js/script.js
@@ -0,0 +1,70 @@
+const submitLink = document.getElementById("submitLink");
+const demo = document.getElementById("demo");
+const data = document.getElementById("data");
+
+submitLink.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    var webLink = document.getElementById("webLink");
+    const file = webLink.value;
+
+    if(file != ""){
+        getText(file);
+    }
+});
+
+// Funkcija koja dohvaća sadržaj sa stranice, uklanja skripte, stilove i navigacijske elemente te vraća čisti tekstualni sadržaj tijela dokumenta.
+async function getText(file) {
+    let y = await fetchText(file);
+  
+    const doc = parseHTML(y);
+
+    const elementsToDelete = ["script", "nav", "sidenav", "topnav", "pagetop", "style", "header", "changelog", "navigation", "sidebar", "footer"];
+
+    deleteElementsByPartialName(doc, elementsToDelete);
+
+    console.log(doc.body.textContent);
+
+    const textContent = doc.body.innerHTML;
+    // console.log(textContent);
+    demo.innerHTML = textContent;
+    data.innerHTML = doc.body.textContent;
+  
+    // // Primjer korištenja funkcije za pronalaženje elemenata s dijelom imena "navigation"
+    // const navigationElements = findElementsByPartialName(doc, "navigation");
+    // console.log(navigationElements);
+  }
+  
+  // Funkcija koja dohvaća tekstualni sadržaj preko Fetch API-a
+  async function fetchText(file) {
+    let x = await fetch(file);
+    return await x.text();
+  }
+  
+  // Funkcija koja pretvara HTML string u DOM elemente pomoću DOMParser-a
+  function parseHTML(htmlString) {
+    const parser = new DOMParser();
+    return parser.parseFromString(htmlString, "text/html");
+  }
+
+  function findElementsByPartialName(doc, name) {
+    const htmlElements = doc.querySelectorAll(`${name}`);
+    const classElements = doc.querySelectorAll(`[class*="${name}"]`);
+    const idElements = doc.querySelectorAll(`[id*="${name}"]`);
+    const elements = new Set([...htmlElements, ...classElements, ...idElements]);
+    
+    return Array.from(elements);
+  }
+
+  function deleteElementsByPartialName(doc, names) {
+    names.forEach(name => {
+      const htmlElements = doc.querySelectorAll(`${name}`);
+      const classElements = doc.querySelectorAll(`[class*="${name}"]`);
+      const idElements = doc.querySelectorAll(`[id*="${name}"]`);
+      const elements = new Set([...htmlElements, ...classElements, ...idElements]);
+  
+      elements.forEach(element => {
+        element.remove();
+      });
+    });
+  }
--- JS/server.js
+++ JS/server.js
@@ -1,39 +1,44 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const ds = require('fs');
 const swaggerUi = require('swagger-ui-express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
 const port = 8000;
+const putanja = __dirname;
+const cors = require('cors');
 
 // Putanja do JSON datoteke s informacijama o API-ju
 const apiInfoFilePath = path.join(__dirname, 'api-info.json');
 
 // Funkcija za čitanje JSON datoteke
-function readApiInfoFromFile() {
-  const rawData = fs.readFileSync(apiInfoFilePath);
-  return JSON.parse(rawData);
+async function readApiInfoFromFile() {
+  try {
+    const rawData = await fs.promises.readFile(apiInfoFilePath);
+    return JSON.parse(rawData);
+  } catch (error) {
+    console.error('Greška prilikom čitanja JSON datoteke:', error);
+    return null;
+  }
 }
 
-// Generiranje Swagger dokumentacije iz JSON objekta
-const apiInfo = readApiInfoFromFile();
-
-// Generiranje Swagger dokumentacije iz JSON objekta
-const swaggerDocument = {
-  openapi: '3.0.0',
-  info: {
-    title: apiInfo.apiName,
-    description: apiInfo.description,
-    version: '1.0.0'
-  },
-  paths: generatePaths(apiInfo.routes),
-  servers: [
-    {
-      url: `http://localhost:${port}`,
+async function generateSwaggerDocument(apiInfo) {
+  return {
+    openapi: '3.0.0',
+    info: {
+      title: apiInfo.apiName,
+      description: apiInfo.description,
+      version: '1.0.0'
     },
-  ],
-  // Dodajte ostale dijelove Swagger dokumentacije prema potrebi
-};
+    paths: generatePaths(apiInfo.endpoints),
+    servers: [
+      {
+        url: `http://localhost:${port}`,
+      },
+    ],
+    // Dodajte ostale dijelove Swagger dokumentacije prema potrebi
+  };
+}
 
 // Funkcija za generiranje dijela "paths" Swagger dokumentacije
 function generatePaths(routes) {
@@ -60,10 +65,36 @@ function generatePaths(routes) {
   
     return paths;
 }
-  
+console.log(path.join(putanja, "index.html"));
 
-// Ruta za prikaz Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cors());
+
+app.use("/js",express.static(putanja+"/js"));
+
+app.get("/",(zahtjev, odgovor) => {
+  var pocetna = ds.readFileSync(path.join(putanja, "index.html"));
+  odgovor.type("html");
+  odgovor.write(pocetna);
+  odgovor.send();
+});
+
+app.use('/api-docs', swaggerUi.serve);
+// Ruta za prikaz Swagger UI, koristimo asinkronu funkciju
+app.get('/api-docs', async (zahtjev, odgovor) => {
+  try {
+    // Pročitajte JSON objekt iz datoteke api-info.json
+    const apiInfo = await readApiInfoFromFile();
+
+    // Generirajte Swagger dokumentaciju iz pročitanog JSON objekta
+    const swaggerDocument = await generateSwaggerDocument(apiInfo);
+
+    // Prikaz Swagger UI pomoću pročitanog Swagger dokumenta
+    swaggerUi.setup(swaggerDocument)(zahtjev, odgovor);
+  } catch (error) {
+    console.error('Greška prilikom generiranja Swagger dokumentacije:', error);
+    odgovor.status(500).send('Greška prilikom generiranja Swagger dokumentacije.');
+  }
+});
 
 // Pokretanje servera
 app.listen(port, () => {
