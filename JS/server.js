0b4173f86683fd740a3c03731546303e44c3f1d7
mtomasic2 <mtomasic20@student.foi.hr>
2023-07-28 12:01:35 +0200

    Generira se JSON file za OpenAPI specifikaciju

--- JS/server.js
+++ JS/server.js
@@ -1,5 +1,3 @@
-const { Configuration, OpenAIApi } = require("openai");
-
 const express = require('express');
 const ds = require('fs');
 const bodyParser = require('body-parser');
@@ -11,20 +9,23 @@ const port = 8000;
 const putanja = __dirname;
 const cors = require('cors');
 
-const webParser = require("./webParser.js");
-
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
+const {encode, decode} = require('gpt-3-encoder')
+const webParser = require("./webParser.js");
+
 // Putanja do JSON datoteke s informacijama o API-ju
 const apiInfoFilePath = path.join(__dirname, 'api-info2.json');
+const filePath = path.join(__dirname, 'api-info3.json');
 
 // Funkcija za čitanje JSON datoteke
-async function readApiInfoFromFile() {
+async function readApiInfoFromFile(filePath) {
   try {
-    const rawData = await fs.promises.readFile(apiInfoFilePath);
+    const rawData = await fs.promises.readFile(filePath);
     return JSON.parse(rawData);
   } catch (error) {
     console.error('Greška prilikom čitanja JSON datoteke:', error);
@@ -32,46 +33,64 @@ async function readApiInfoFromFile() {
   }
 }
 
-async function generateSwaggerDocument(apiInfo) {
-  return {
-    openapi: '3.0.0',
-    info: {
-      title: apiInfo.apiName,
-      description: apiInfo.description,
-      version: '1.0.0'
-    },
-    paths: generatePaths(apiInfo.endpoints),
-    servers: apiInfo.servers.map(url => ({ url }))
-    
-    // Dodajte ostale dijelove Swagger dokumentacije prema potrebi
-  };
-}
-
-// Funkcija za generiranje dijela "paths" Swagger dokumentacije
-function generatePaths(routes) {
-    const paths = {};
-  
-    for (const route of routes) {
-      const pathObj = {
-        [route.method.toLowerCase()]: {
-          summary: route.description,
-          // Dodajte druge dijelove ruta prema potrebi
-        }
-      };
-  
-      if (route.parameters && route.parameters.length > 0) {
-        pathObj[route.method.toLowerCase()].parameters = route.parameters;
-      }
-
-      if (route.responses && Object.keys(route.responses).length > 0) {
-        pathObj[route.method.toLowerCase()].responses = route.responses;
-      }
-  
-      paths[route.path] = pathObj;
+// Funkcija za pisanje stringa u datoteku
+function writeToFile(filePath, content) {
+  fs.writeFile(filePath, content, (err) => {
+    if (err) {
+      console.error('Greška prilikom pisanja u datoteku:', err);
+    } else {
+      console.log('String je uspješno zapisan u datoteku.');
     }
-  
-    return paths;
+  });
 }
+function countTokens(text) {
+  const encoded = encode(text)
+  return encoded.length;
+}
+
+async function getChatCompletionResponse(chatPrompt) {
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "system", content: `${chatPrompt}` }],
+    });
+
+    // Dohvatite odgovor od API-ja
+    const odgovorApi = completion.data.choices[0].message.content;
+
+    return odgovorApi;
+  } catch (error) {
+    console.error('Greška prilikom slanja zahtjeva:', error);
+    throw error;
+  }
+}
+
+function extractJsonFromString(inputString) {
+  const startIdx = inputString.indexOf('{');
+  const endIdx = inputString.lastIndexOf('}');
+  
+  if (startIdx === -1 || endIdx === -1 || endIdx < startIdx) {
+    throw new Error('Invalid JSON string');
+  }
+  
+  const jsonString = inputString.slice(startIdx, endIdx + 1);
+  
+  try {
+    const jsonObject = JSON.parse(jsonString);
+    return jsonObject;
+  } catch (error) {
+    throw new Error('Failed to parse JSON object');
+  }
+}
+
+async function writeApiInfoToFile(chatPrompt) {
+  const chatResponse = await getChatCompletionResponse(chatPrompt);
+  console.log(chatResponse);
+  const extractedJson = extractJsonFromString(chatResponse);
+  console.log(extractedJson);
+  writeToFile(filePath, JSON.stringify(extractedJson));
+}
+
 console.log(path.join(putanja, "index.html"));
 
 app.use(cors());
@@ -93,29 +112,25 @@ app.get("/",(zahtjev, odgovor) => {
 
 app.post("/api", async (zahtjev, odgovor) => {
   try {
-    // const completion = await openai.createChatCompletion({
-    //   model: "gpt-3.5-turbo",
-    //   messages: [{role: "system", content: "Say hi to the GPT-3 chatbot!"}],
-    // });
-
-    // // Dohvatite odgovor od API-ja
-    // const odgovorApi = completion.data.choices[0].message;
-
-    // // // Parsirajte odgovor u JSON objekt
-    // // const jsonData = JSON.parse(odgovorApi);
-
-    // // Vratite JSON objekt kao odgovor
-    // odgovor.json(odgovorApi);
-
     const webLink = zahtjev.body.webLink;
-    odgovor.send(webLink);
 
     const webparser = new webParser();
-    console.log(await webparser.getText(webLink));
-    
+    const parsedText = await webparser.getText(webLink);
+    //console.log(countTokens(parsedText));
+
+    const chatPrompt = "Give me example of OpenAPI specification in JSON file";
+    await writeApiInfoToFile(chatPrompt);
+
+    // const apiInfo = await readApiInfoFromFile(filePath);
+    // console.log(apiInfo);
+
+    odgovor.redirect("/api-docs");
+    return;
+
   } catch (error) {
     console.error('Greška prilikom slanja zahtjeva:', error);
     odgovor.status(500).json({ error: 'Greška prilikom slanja zahtjeva' });
+    return;
   }
 });
 
@@ -124,11 +139,7 @@ app.use('/api-docs', swaggerUi.serve);
 app.get('/api-docs', async (zahtjev, odgovor) => {
   try {
     // Pročitajte JSON objekt iz datoteke api-info.json
-    const apiInfo = await readApiInfoFromFile();
-
-    // Generirajte Swagger dokumentaciju iz pročitanog JSON objekta
-    // const swaggerDocument = await generateSwaggerDocument(apiInfo);
-
+    const apiInfo = await readApiInfoFromFile(filePath);
     const swaggerDocument = apiInfo;
     // console.log(apiInfo);
 
